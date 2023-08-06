0dfb5293773268cce7232015c7063df435c484fe
thaiminhpv <thaiminhpv@gmail.com>
2023-08-06 18:56:43 +0700

    switch users during github action runner setup

--- scripts/gcp-scripts/github-action-worker.sh
+++ scripts/gcp-scripts/github-action-worker.sh
@@ -9,6 +9,8 @@ else
     token=$1
 fi
 
+sudo su - thaiminhpv
+
 mkdir -p ~/devops/actions-runner/k8s-manifest && cd ~/devops/actions-runner/k8s-manifest
 curl -o actions-runner-linux-x64-2.307.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.307.1/actions-runner-linux-x64-2.307.1.tar.gz
 echo "038c9e98b3912c5fd6d0b277f2e4266b2a10accc1ff8ff981b9971a8e76b5441  actions-runner-linux-x64-2.307.1.tar.gz" | shasum -a 256 -c
