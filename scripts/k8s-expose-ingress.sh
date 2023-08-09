709753b5a0cf9b11b6722d657cf89b61d9ad9415
thaiminhpv <thaiminhpv@gmail.com>
2023-08-10 01:31:22 +0700

    add parralel expose

--- scripts/k8s-expose-ingress.sh
+++ scripts/k8s-expose-ingress.sh
@@ -4,7 +4,7 @@ set -x
 # curl -s https://raw.githubusercontent.com/EFISS-Capstone-SU23/k8s-manifest/main/scripts/k8s-expose-ingress.sh | bash
 
 cat <<'EOF' > /home/thaiminhpv/expose-ingress.sh
-sudo -E kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80 --address='0.0.0.0'
+sudo -E kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80 --address='0.0.0.0' &
 sudo -E kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 443:443 --address='0.0.0.0'
 EOF
 
