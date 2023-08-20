88e3a65b4b842f50571b7919a1a7e42f861b50be
thaiminhpv <thaiminhpv@gmail.com>
2023-08-20 20:32:56 +0700

    add calico custom script

--- scripts/gcp-scripts/init-kube-network.sh
+++ scripts/gcp-scripts/init-kube-network.sh
@@ -22,3 +22,10 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
 # kubeadm token create --print-join-command
+
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+# curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+# kubectl create -f custom-resources.yaml
+# watch kubectl get pods -n calico-system
+
+
