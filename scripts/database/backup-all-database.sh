3a307eb0e2d49c4b0bf4fa637c4f3c225e5dbdf7
thaiminhpv <thaiminhpv@gmail.com>
2023-08-21 08:53:18 +0700

    update backup db scripts

--- scripts/database/backup-all-database.sh
+++ scripts/database/backup-all-database.sh
@@ -2,10 +2,10 @@
 
 read -sp "Enter password: " password
 
-services=("efiss_ads" "efiss_auth" "efiss_admin" "efiss_normal_user")
+services=("ads" "auth" "admin" "normal-user")
 
 for service in "${services[@]}"; do
-    mysqldump -h mysql.efiss.tech --user=root -P 3306 -p"$password" --single-transaction=TRUE --set-gtid-purged=OFF --databases $service > $service.sql 
+    # mysqldump -h $service.db.efiss.tech --user=root -P 3306 -p"$password" --single-transaction=TRUE --set-gtid-purged=OFF --databases efiss_$service > $service.sql 
+    mysql -h $service.db.efiss.tech --user=root -P 3306 -p"$password" < $service.sql
 done
 
-# mysql -h mysql.efiss.tech --user=root -P 3306 -p"$password" < $service.sql
