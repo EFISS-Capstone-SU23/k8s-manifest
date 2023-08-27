65b714b56c35805354ad62be3b0d47d76fd6c378
thaiminhpv <thaiminhpv@gmail.com>
2023-08-27 16:17:26 +0700

    add local scripts

--- scripts/database/backup-all-database.sh
+++ scripts/database/backup-all-database.sh
@@ -2,10 +2,12 @@
 
 read -sp "Enter password: " password
 
-services=("ads" "auth" "admin" "normal-user")
+services=("ads" "auth" "admin")
 
 for service in "${services[@]}"; do
-    # mysqldump -h $service.db.efiss.tech --user=root -P 3306 -p"$password" --single-transaction=TRUE --set-gtid-purged=OFF --databases efiss_$service > $service.sql 
-    mysql -h $service.db.efiss.tech --user=root -P 3306 -p"$password" < $service.sql
+    mysqldump -h $service.db.efiss.tech --user=root -P 3306 -p"$password" --single-transaction=TRUE --set-gtid-purged=OFF --databases efiss_$service > $service.sql 
+    # mysql -h $service.db.efiss.tech --user=root -P 3306 -p"$password" < $service.sql
 done
 
+service="normal-user"
+mysqldump -h $service.db.efiss.tech --user=root -P 3306 -p"$password" --single-transaction=TRUE --set-gtid-purged=OFF --databases efiss_normal_user > $service.sql 
